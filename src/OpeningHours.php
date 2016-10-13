a3f1c9ca62cae7d18a79d6ee1d7bbccfc5a42d87
Jérôme Gamez <jerome@gamez.name>
2016-10-13 10:19:28 +0200

    Change variable name for iterated opening hours

--- src/OpeningHours.php
+++ src/OpeningHours.php
@@ -53,8 +53,8 @@ class OpeningHours
     {
         list($openingHours, $exceptions) = $this->parseOpeningHoursAndExceptions($data);
 
-        foreach ($openingHours as $day => $openingHours) {
-            $this->setOpeningHoursFromStrings($day, $openingHours);
+        foreach ($openingHours as $day => $openingHoursForThisDay) {
+            $this->setOpeningHoursFromStrings($day, $openingHoursForThisDay);
         }
 
         $this->setExceptionsFromStrings($exceptions);
