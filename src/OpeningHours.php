a047192325ad5a968cc164fdc281e9905f6f0d2a
Kyle <kylekatarnls@users.noreply.github.com>
2021-12-09 09:42:39 +0100

    Add array-shape create() PHPDoc

--- src/OpeningHours.php
+++ src/OpeningHours.php
@@ -61,7 +61,17 @@ class OpeningHours
     }
 
     /**
-     * @param  string[][]  $data
+     * @param  array{
+     *             monday: array<string|array>,
+     *             tuesday: array<string|array>,
+     *             wednesday: array<string|array>,
+     *             thursday: array<string|array>,
+     *             friday: array<string|array>,
+     *             saturday: array<string|array>,
+     *             sunday: array<string|array>,
+     *             exceptions: array<array<string|array>>,
+     *             filters: callable[],
+     *         }                         $data
      * @param  string|DateTimeZone|null  $timezone
      * @return static
      */
@@ -119,7 +129,17 @@ class OpeningHours
     }
 
     /**
-     * @param  string[][]  $data
+     * @param  array{
+     *             monday: array<string|array>,
+     *             tuesday: array<string|array>,
+     *             wednesday: array<string|array>,
+     *             thursday: array<string|array>,
+     *             friday: array<string|array>,
+     *             saturday: array<string|array>,
+     *             sunday: array<string|array>,
+     *             exceptions: array<array<string|array>>,
+     *             filters: callable[],
+     *         }                         $data
      * @param  string|DateTimeZone|null  $timezone
      * @return static
      */
